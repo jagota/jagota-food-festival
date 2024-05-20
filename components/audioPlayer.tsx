fc0681ce91ba9f171dd0ed004caa4f4e0ab4b2ca
Mritunjay <mupadhyay00@gmail.com>
2024-05-20 11:46:01 +0700

    feat: Style camera and webcam

--- components/audioPlayer.tsx
+++ components/audioPlayer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { HiOutlineSpeakerWave } from "react-icons/hi2";
 import cntl from 'cntl';
 
@@ -9,6 +9,9 @@ const classes = {
         items-center
         space-x-4
         mt-4
+    `,
+    audio: cntl`
+        hidden
     `,
     button: (isPlaying: boolean) => cntl`
         w-24
@@ -27,27 +30,39 @@ const classes = {
         h-8
         ${isPlaying ? 'animate-pulse' : ''}
     `,
-    
+    text: cntl`text-center mt-2 text-white text-xl`
 };
 export const AudioPlayerComponent = ({ url }: { url: string }) => {
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isPlaying, setPlaying] = useState(false)
 
   const play = () => {
     audioRef.current?.play();
+    setPlaying(true);
   };
+
+  const pause = () => {
+    audioRef.current?.pause();
+    setPlaying(false);
+  };
+
+  const handleClick = () => {
+    if (isPlaying) {
+      pause();
+    } else {
+      play();
+    }
+  }
 
   return (
     <div className="audio-player">
-      <audio ref={audioRef} src={url} controls></audio>
-      <button className="w-24 h-24 flex justify-center items-center rounded-full border-white border border-2 animate-recording" onClick={play}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 animate-pulse">
-  <path d="M8.25 4.5a3.75 3.75 0 1 1 7.5 0v8.25a3.75 3.75 0 1 1-7.5 0V4.5Z" />
-  <path d="M6 10.5a.75.75 0 0 1 .75.75v1.5a5.25 5.25 0 1 0 10.5 0v-1.5a.75.75 0 0 1 1.5 0v1.5a6.751 6.751 0 0 1-6 6.709v2.291h3a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1 0-1.5h3v-2.291a6.751 6.751 0 0 1-6-6.709v-1.5A.75.75 0 0 1 6 10.5Z" />
-</svg>
-
+      <audio className={classes.audio} ref={audioRef} src={url} controls></audio>
+      <button className={classes.button(isPlaying)} onClick={handleClick}>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={classes.svg(isPlaying)}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+      </svg>
       </button>
-      
-
+      <p className={classes.text}>{isPlaying ? 'Playing' : 'Paused'}</p>
     </div>
   );
 };
--- components/audioRecorder.tsx
+++ components/audioRecorder.tsx
@@ -3,6 +3,29 @@ import { dataUrlToFile } from "@/lib/base64ToImage";
 import { uploadFile } from "@/lib/getSignedUrl";
 import { useState, useRef, useEffect } from "react";
 import { AudioPlayerComponent } from "./audioPlayer";
+import cntl from 'cntl';
+import { PrimaryButton } from "./ui/primary-button";
+
+const classes = {
+    recordingButton: (isPlaying: boolean) => cntl`
+        w-24
+        h-24
+        flex
+        justify-center
+        items-center
+        rounded-full
+        border-white
+        border
+        border-2
+        ${isPlaying ? 'animate-recording' : ''}
+    `,
+    recordingSvg: (isPlaying: boolean) => cntl`
+        w-8
+        h-8
+        ${isPlaying ? 'animate-pulse' : ''}
+    `,
+    text: cntl`text-center text-white text-xl`
+};
 
 const mimeType = "audio/webm";
 interface IAudioRecorderComponentProps {
@@ -25,10 +48,6 @@ export const AudioRecorderComponent = ({
   const [audio, setAudio] = useState<string | null>(null);
 
   const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
-
-  const play = () => {
-    audioRef.current?.play();
-  };
 
   useEffect(() => {
     getMicrophonePermission();
@@ -86,7 +105,15 @@ export const AudioRecorderComponent = ({
     };
   };
 
-  const handleClick = async () => {
+  const handleClick = () => {
+    if (recordingStatus === "recording") {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+  }
+
+  const save = async () => {
     if (!audio) return;
     console.log("audio", audio);
     const fileName = `${Date.now() + Math.random()}-audio-recording.webm`;
@@ -99,37 +126,40 @@ export const AudioRecorderComponent = ({
     return url;
   };
 
+  const retake = () => {
+    setAudio(null);
+  };
+
   return (
     <div className="fixed top-0 bottom-0 min-w-full min-h-screen">
+      
       <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-neutral-50 to-violet-300">
-        <div className="shadow-xl bg-gradient-to-r to-cyan-500 from-blue-500 md:w-[400px] w-[95%] px-[3rem] py-[5rem] text-white">
-          <div className="audio-controls ">
-            {permission && recordingStatus === "inactive" ? (
-              <button onClick={startRecording} type="button">
-                Start Recording
-              </button>
-            ) : null}
-            {recordingStatus === "recording" ? (
-              <button onClick={stopRecording} type="button">
-                Stop Recording
-              </button>
-            ) : null} 
-          </div>
+        <div className="relative shadow-xl min-h-[400px] bg-gradient-to-r to-cyan-500 from-blue-500 md:w-[400px] w-[95%] px-[2rem] py-[3.5rem] text-white">
+        <button className="absolute left-2 top-2 p-1 bg-transparent" onClick={onClose}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+
+        </button>
           {audio ? (
-            <div className="audio-player">
+            <div className="audio-player flex flex-col items-center gap-10">
                 <AudioPlayerComponent url={audio} />
-              {/* <audio ref={audioRef} src={audio} controls></audio>
-              <a download href={audio}>
-                Download Recording
-              </a>
-              <button className="w-20 h-8 bg-cyan" onClick={play}>
-                play
-              </button> */}
-              <button className="w-20 h-8 bg-cyan" onClick={handleClick}>
-                save
-              </button>
+            <div className="flex flex-row w-full justify-around mt-2">
+              <PrimaryButton buttonText="Save" onClick={save} />
+              <PrimaryButton buttonText="Retake" onClick={retake} />
             </div>
-          ) : null}
+            </div>
+          ) : (
+            <div className="audio-controls flex flex-col items-center gap-10">
+            <button className={classes.recordingButton(recordingStatus === 'recording')} onClick={handleClick}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={classes.recordingSvg(recordingStatus === 'recording')}>
+                <path d="M8.25 4.5a3.75 3.75 0 1 1 7.5 0v8.25a3.75 3.75 0 1 1-7.5 0V4.5Z" />
+                <path d="M6 10.5a.75.75 0 0 1 .75.75v1.5a5.25 5.25 0 1 0 10.5 0v-1.5a.75.75 0 0 1 1.5 0v1.5a6.751 6.751 0 0 1-6 6.709v2.291h3a.75.75 0 0 1 0 1.5h-7.5a.75.75 0 0 1 0-1.5h3v-2.291a6.751 6.751 0 0 1-6-6.709v-1.5A.75.75 0 0 1 6 10.5Z" />
+              </svg>
+            </button>
+            <p className={classes.text}>{recordingStatus === 'recording' ? 'Recording' : 'Not Recording'}</p>
+          </div>
+          )}
         </div>
       </div>
     </div>
