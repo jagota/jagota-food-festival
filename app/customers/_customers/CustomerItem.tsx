bb0df7ad764d1452d858447a0b2c409f9a708ea4
Mritunjay <mupadhyay00@gmail.com>
2024-05-23 10:47:16 +0700

    feat Show Added customer

--- app/customers/_customers/CustomerItem.tsx
+++ app/customers/_customers/CustomerItem.tsx
@@ -1,29 +1,51 @@
-import { ICustomerToDB } from "@/interfaces/Customer.interface"
+import LetterAvatar from "@/components/LetterAvatar";
+import { CustomerInterface } from "@/interfaces/Customer.interface";
 
 interface Props {
-    customer: ICustomerToDB
-}
-const CustomerItem = ({ customer }: Props) => {
-    return (
-        <div className="text-sm leading-6">
-    <figure className="relative flex flex-col-reverse bg-slate-100 rounded-lg p-6 dark:bg-slate-800 dark:highlight-white/5">
-        <blockquote className="mt-6 text-slate-700 dark:text-slate-300">
-            <p>I may not show it but i cant live without {product}.</p>
-        </blockquote>
-        <figcaption className="flex items-center space-x-4">
-            <img src="https://images.unsplash.com/photo-1632910121591-29e2484c0259?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxjb2RlcnxlbnwwfDB8fHwxNzEwMTY0NjIzfDA&ixlib=rb-4.0.3&q=80&w=1080" alt="" className="flex-none w-14 h-14 rounded-full object-cover" loading="lazy" decoding="async">
-            <div className="flex-auto">
-                <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
-                    John Doe
-                </div>
-                <div className="mt-0.5 dark:text-slate-300">
-                    Web Developer
-                </div>
-            </div>
-        </figcaption>
-    </figure>
-</div>
-    )
+  customer: CustomerInterface;
 }
 
-export { CustomerItem }
+
+const renderItems = (items: string[]) => {
+    return items.map((item, index) => {
+        return (<p key={item}>{item}</p>);
+    });
+};
+const renderBlock = (title: string, items: string[]) => {
+    return (
+        <div className="mt-6 text-slate-700 dark:text-slate-300 ">
+            <h4 className="text-base text-slate-900 font-semibold">{title}</h4>
+           <div className="flex flex-wrap gap-2">
+            {renderItems(items)}
+           </div>
+        </div>
+    )
+};
+const CustomerItem = ({ customer }: Props) => {
+    const { interested_in, shop_type} = customer;
+  return (
+    <div className="text-sm leading-6">
+      <figure
+        className="relative flex flex-col bg-slate-200 rounded-lg p-6 
+            dark:bg-slate-800 dark:highlight-white/5"
+      >
+        <figcaption className="flex space-x-4">
+          <LetterAvatar name={customer.name} />
+          <div className="flex-auto">
+            <div className="text-base text-slate-900 font-semibold dark:text-slate-200">
+              {customer.name}
+            </div>
+            <div className="mt-0.5 dark:text-slate-300">{customer.mobile}</div>
+            <div className="mt-0.5 dark:text-slate-300">
+              {customer.province} - {customer.district}
+            </div>
+          </div>
+        </figcaption>
+        {interested_in.length > 0 && renderBlock("Interests", interested_in)}
+        {shop_type.length > 0 && renderBlock("Customer Shop", shop_type)}
+      </figure>
+    </div>
+  );
+};
+
+export { CustomerItem };
