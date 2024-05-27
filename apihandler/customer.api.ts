58f6290752e78f346d0ca57563e19bfc262f555c
Mritunjay <mupadhyay00@gmail.com>
2024-05-27 21:46:51 +0700

    🔧 refactor(api): remove redundant withCredentials option from Axios instances in customer, event, and upload APIs

--- apihandler/customer.api.ts
+++ apihandler/customer.api.ts
@@ -3,7 +3,6 @@ import axios from 'axios'
 // Create an Axios instance for API requests
 const apiClient = axios.create({
     baseURL: process.env.NEXT_PUBLIC_SEVER_API_URL,
-    withCredentials: true,
     timeout: 120000,
 })
 
