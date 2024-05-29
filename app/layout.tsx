ea15016087f9c0ce1af0927aad83ea4379bffe7a
Mritunjay <mupadhyay00@gmail.com>
2024-05-29 10:58:07 +0700

    ✨ feat(layout.tsx): add viewport metadata to improve responsiveness and user experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
@@ -13,6 +13,15 @@ export const metadata: Metadata = {
   title: "Jagota Events",
   description: "Jagota Food festival",
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  // Also supported by less commonly used
+  // interactiveWidget: 'resizes-visual',
+}
 
 export default function RootLayout({
   children,
